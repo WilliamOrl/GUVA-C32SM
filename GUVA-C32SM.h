8f73973391a5330e5975afe856569528db1de798
WilliamOrl <62764335+WilliamOrl@users.noreply.github.com>
2020-07-31 20:54:17 -0300

    Update GUVA-C32SM.h

--- GUVA-C32SM.h
+++ GUVA-C32SM.h
@@ -6,17 +6,18 @@ extern "C"	{
 
 #include "stm32f4xx_hal.h"
 
-//typedef struct _GUVAC32SM{
-//
-//} GUVAC32SM;
 
+//External Function
 HAL_StatusTypeDef Start_GUVA_C32 (I2C_HandleTypeDef * hi2c);
 HAL_StatusTypeDef Mode_GUVA_C32 (I2C_HandleTypeDef * hi2c, uint8_t MODE, uint8_t PMODE);
 HAL_StatusTypeDef Resolution_GUVA_C32 (I2C_HandleTypeDef * hi2c, uint8_t RES);
-HAL_StatusTypeDef Range_GUVA_C32 ();
-HAL_StatusTypeDef Sleep_GUVA_C32 ();
-HAL_StatusTypeDef Reset_GUVA_C32 ();
-HAL_StatusTypeDef Receive_GUVA_C32 ();
+HAL_StatusTypeDef Range_GUVA_C32 (I2C_HandleTypeDef * hi2c, uint8_t RANGE);
+HAL_StatusTypeDef Sleep_GUVA_C32 (I2C_HandleTypeDef * hi2c, uint8_t TIME);
+HAL_StatusTypeDef Reset_GUVA_C32 (I2C_HandleTypeDef * hi2c);
+HAL_StatusTypeDef Receive_GUVA_C32 (I2C_HandleTypeDef * hi2c, float *UVA);
+
+//Internal Function
+float Converter (uint16_t UVA_RES);
 
 #ifdef _cplusplus
 }
