6cda5f4163a2422427c1f52af67753c20d10e5dd
WilliamOrl <62764335+WilliamOrl@users.noreply.github.com>
2020-07-31 18:33:49 -0300

    Update GUVA-C32SM.h

--- GUVA-C32SM.h
+++ GUVA-C32SM.h
@@ -6,13 +6,13 @@ extern "C"	{
 
 #include "stm32f4xx_hal.h"
 
-typedef struct _GUVAC32SM{
-
-} GUVAC32SM;
+//typedef struct _GUVAC32SM{
+//
+//} GUVAC32SM;
 
 HAL_StatusTypeDef Start_GUVA_C32 (I2C_HandleTypeDef * hi2c);
 HAL_StatusTypeDef Mode_GUVA_C32 (I2C_HandleTypeDef * hi2c, uint8_t MODE, uint8_t PMODE);
-HAL_StatusTypeDef Resolution_GUVA_C32 ();
+HAL_StatusTypeDef Resolution_GUVA_C32 (I2C_HandleTypeDef * hi2c, uint8_t RES);
 HAL_StatusTypeDef Range_GUVA_C32 ();
 HAL_StatusTypeDef Sleep_GUVA_C32 ();
 HAL_StatusTypeDef Reset_GUVA_C32 ();
