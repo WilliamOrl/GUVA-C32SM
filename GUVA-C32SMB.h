e3b58ae5324a2ee64f2e804f41d77a588481bd98
WilliamOrl <62764335+WilliamOrl@users.noreply.github.com>
2020-08-12 17:49:26 -0300

    Create GUVA-C32SMB.h

---
+++ GUVA-C32SMB.h
@@ -0,0 +1,24 @@
+#ifndef LIB_GUVA_C32SMB_H
+#define LIB_GUVA_C32SMB_H
+#ifdef _cplusplus
+extern "C"	{
+#endif
+
+#include "stm32f4xx_hal.h"
+
+
+//External Function
+HAL_StatusTypeDef Start_GUVA_C32 (I2C_HandleTypeDef * hi2c);
+HAL_StatusTypeDef Mode_GUVA_C32 (I2C_HandleTypeDef * hi2c, uint8_t MODE, uint8_t PMODE);
+HAL_StatusTypeDef Resolution_GUVA_C32 (I2C_HandleTypeDef * hi2c, uint8_t RES);
+HAL_StatusTypeDef Sleep_GUVA_C32 (I2C_HandleTypeDef * hi2c, uint8_t TIME);
+HAL_StatusTypeDef Reset_GUVA_C32 (I2C_HandleTypeDef * hi2c);
+HAL_StatusTypeDef Receive_GUVA_C32 (I2C_HandleTypeDef * hi2c, uint8_t RANGE, float *UVA);
+
+//Internal Function
+float Converter (uint16_t UVA_RES, uint8_t RANGE);
+
+#ifdef _cplusplus
+}
+#endif
+#endif// LIB_GUVA_C32SM_H
